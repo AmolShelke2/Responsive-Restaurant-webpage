1dd6e9be5e43a2ee199c9af19d40aebe969ab373
Amol shelke <shelkeamol897@gmail.com>
2022-09-11 21:58:48 +0530

    Added Loader Feature before loading website

--- js/script.js
+++ js/script.js
@@ -16,3 +16,13 @@ window.onscroll = () => {
     document.querySelector("#scroll-top").classList.remove("active");
   }
 };
+
+function loader() {
+  document.querySelector(".loader-container").classList.add("fade-out");
+}
+
+function fadeOut() {
+  setInterval(loader, 3000);
+}
+
+window.onload = fadeOut();
