8eb8ccc595b8a9654f89a26caa06db59fd6ba5de
Amol shelke <shelkeamol897@gmail.com>
2022-09-11 21:40:47 +0530

    Added Back to top button with JS logic

--- js/script.js
+++ js/script.js
@@ -9,4 +9,10 @@ menu.addEventListener("click", () => {
 window.onscroll = () => {
   menu.classList.remove("fa-times");
   navbar.classList.remove("active");
+
+  if (window.scrollY > 60) {
+    document.querySelector("#scroll-top").classList.add("active");
+  } else {
+    document.querySelector("#scroll-top").classList.remove("active");
+  }
 };
